63efe200f3a74fb848b48741384772e48e945c91
Dvalin23 <ayoolazeez2331@gmail.com>
2026-09-03 20:40:02 +0100

    done with js

--- string_nd_num_mehod.js
+++ string_nd_num_mehod.js
@@ -31,3 +31,25 @@ console.log(extract.slice(-3))
 console.log(extract.substring(0,3))
 console.log(extract.substring(3))
 console.log(extract.substring(-3))
+
+//replace
+const text = "Ahmad ia sexy guy, Ahmad is cool and social, Ahmad is tech guy"
+console.log(text.replace("Ahmad", "Conard"))
+console.log(text.replaceAll("Ahmad", "Conard"))
+
+//split
+const testsplit = text.split(",")
+console.log(testsplit)
+
+// reverse
+
+let palindrom = "madam"
+let reversed = palindrom.split("").reverse().join("")
+console.log(reversed)
+
+function check(word){
+    return word == word.split("").reverse().join("") ? "yes its a polinderum" : "No its not a polinderum"
+
+}
+
+console.log(check("Ahmad"))
